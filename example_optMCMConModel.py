8fee8e86b2a79dce36101e84f4fad5b872cace9a
TTang_20 <yiyit62@gmail.com>
2018-03-19 19:57:19 -0400

    Read function script and add power as variable

--- example_optMCMConModel.py
+++ example_optMCMConModel.py
@@ -1,6 +1,6 @@
 # Imports
-import sys
-sys.path.insert(0, '/home/tang/Documents/tulipBin/py')
+import sys,os
+sys.path.insert(0, '/home/tang/Documents/tulipBIN/py')
 # Import UQ Library
 import tulipUQ as uq
 # Import Computational Model Library
@@ -12,34 +12,24 @@ import tulipAC as ac
 # Import numpy and matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-from constants import *
+import time
 
+t1=time.process_time()
 # MAIN FUNCTION
-def main(modelType):
+def main(fileName,power):
 
   # Set Model
-  if(modelType == kPointAndCircle):
-    model = cm.cmAnalyticalExpressionModel(cm.kPointAndCircle)
-  elif(modelType == kAlphaCurve):
-    model = cm.cmAnalyticalExpressionModel(cm.kAlphaCurve)
-  elif(modelType == kKuramoto):
-    model = cm.cmAnalyticalExpressionModel(cm.kKuramoto)
-  elif(modelType == kWhitneyUmbrella):
-    model = cm.cmAnalyticalExpressionModel(cm.kWhitneyUmbrella)
-  elif(modelType == kPosdim):
-    model = cm.cmAnalyticalExpressionModel(cm.kPosdim)
-  elif(modelType == kSeller):
-    model = cm.cmAnalyticalExpressionModel(cm.kSeller)
-  elif(modelType == kEnergyLandscape):
-    model = cm.cmAnalyticalExpressionModel(cm.kEnergyLandscape)
-  elif(modelType == kKuramotoReform):
-  	model = cm.cmAnalyticalExpressionModel(cm.kKuramotoReform)
-  elif(modelType == kEnergyLandscapeopt):
-    model = cm.cmAnalyticalExpressionModel(cm.kEnergyLandscapeopt)
-  else:
-    print('Error: Invalid Model.')
-    sys.exit(-1)
+  model = cm.cmBertiniSolverModel(fileName)
+  modelType = fileName.split("_")[1]
+  # SEt power
+  model.setExponent(power)
+  # add solution
+  # sol=uq.stdVec()
 
+  # for loopA in range(9):
+  #   sol.push_back(0.0)
+  # model.addSolution(sol)
+  
   # Set Optimizer Parameters
   # Convergence Tolerance
   convTol = 1.0e-10
@@ -55,38 +45,19 @@ def main(modelType):
   nm.setModel(model)
 
   # Total Number of iterations
-  totInitialGuess = 100  
+  totInitialGuess = 100
   totRestarts     = 20
 
   # Read MCMC Samples and sub-samples using totInitialGuess
-  if(modelType == kPointAndCircle):
-    data = np.loadtxt('paramTraces_01.txt',skiprows=1)
-  elif(modelType == kAlphaCurve):
-    data = np.loadtxt('paramTraces_02.txt',skiprows=1)
-  elif(modelType == kKuramoto):
-    data = np.loadtxt('paramTraces_03.txt',skiprows=1)
-  elif(modelType == kWhitneyUmbrella):
-    data = np.loadtxt('paramTraces_04.txt',skiprows=1)
-  elif(modelType == kPosdim):
-    data = np.loadtxt('paramTraces_05.txt',skiprows=1)
-  elif(modelType == kSeller):
-    data = np.loadtxt('paramTraces_06.txt',skiprows=1)
-  elif(modelType == kEnergyLandscape):
-    data = np.loadtxt('paramTraces_07.txt',skiprows=1)
-  elif(modelType == kKuramotoReform):
-  	data = np.loadtxt('paramTraces_08.txt',skiprows=1)
-  elif(modelType == kEnergyLandscapeopt):
-    data = np.loadtxt('paramTraces_07.txt',skiprows=1)
-  else:
-    print('Error: Invalid Model.')
-    sys.exit(-1)
-
+  #data = np.loadtxt('paramTraces.txt',skiprows=1)
+  data = np.loadtxt('paramTraces_' + str(modelType),skiprows=1)
   # SubSample
   print(np.random.choice(data.shape[0], totInitialGuess))
   data = data[np.random.choice(data.shape[0], totInitialGuess),2:]
 
   # Loop over the randomly selected initial guess
   results = np.zeros((totInitialGuess,model.getParameterTotal()))
+  Var = np.zeros(totInitialGuess)
   for loopA in range(totInitialGuess):
     # Get Model Limits
     x0 = data[loopA,:]
@@ -109,33 +80,27 @@ def main(modelType):
 
     # Read Optimal Parameters From File
     results[loopA,:] = np.loadtxt('optParams.txt')
+    Var[loopA] = np.loadtxt("optValue.txt")
 
   # Save Final Result
-  if(modelType == kPointAndCircle):
-    np.savetxt('OptimalPoints_01.txt',results)    
-  elif(modelType == kAlphaCurve):
-    np.savetxt('OptimalPoints_02.txt',results)    
-  elif(modelType == kKuramoto):
-    np.savetxt('OptimalPoints_03.txt',results)
-  elif(modelType == kWhitneyUmbrella):
-    np.savetxt('OptimalPoints_04.txt',results)
-  elif(modelType == kPosdim):
-    np.savetxt('OptimalPoints_05.txt',results)
-  elif(modelType == kSeller):
-    np.savetxt('OptimalPoints_06.txt',results)
-  elif(modelType == kEnergyLandscape):
-    np.savetxt('OptimalPoints_07.txt',results)
-  elif(modelType == kKuramotoReform):
-  	np.savetxt('OptimalPoints_08.txt',results)
-  elif(modelType == kEnergyLandscapeopt):
-    np.savetxt('OptimalPoints_07.txt',results)
-  else:
-    print('Error: Invalid Model.')
-    sys.exit(-1)
+  np.savetxt('OptimalPoints_' + str(modelType),results)
+  np.savetxt('OptimalValue_' + str(modelType),Var)
 
+  #calculate cpu time
+  cputime = time.process_time()-t1
+  print(cputime)
+  file = "cputime.txt"
+  if os.path.exists(file):
+    append_write = 'a' # append if already exists
+  else:
+    append_write = 'w' # make a new file if not
+  cpu = open(file,append_write)
+  cpu.write("%s %.f %.5f \n" %(modelType,power,cputime))
+  cpu.close() 
 # ====
 # MAIN
 # ====
 if __name__ == "__main__":
 	
-  main(int(sys.argv[1]))
+  main(sys.argv[1],int(sys.argv[2]))
+  
