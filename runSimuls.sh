7b7639852a6639b049b5d20e8368d8104afcf2b4
TTang_20 <yiyit62@gmail.com>
2018-03-19 20:10:06 -0400

    bash script to run mutiple models

--- runSimuls.sh
+++ runSimuls.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
 # Run MCMC
-mpiexec -n 3 python3 example_runMCMConModel.py 0
-mpiexec -n 3 python3 example_runMCMConModel.py 1
-mpiexec -n 3 python3 example_runMCMConModel.py 2
-mpiexec -n 3 python3 example_runMCMConModel.py 7
-
+#mpiexec -n 3 python3 example_runMCMConModel.py input.txt
 
 # Run Optimization
-python3 example_optMCMConModel.py 0
-python3 example_optMCMConModel.py 1
-python3 example_optMCMConModel.py 2
-python3 example_optMCMConModel.py 7
+#python3 example_optMCMConModel.py input.txt
 
 
-# Plot Results
-python3 showPoints.py
+# for i in 2 4 8 16
+# do 
+	
+# 	mpiexec -n 3 python3 example_runMCMConModel.py input_kseller.txt $((i))
+# 	# Run Optimization
+# 	python3 example_optMCMConModel.py input_kseller.txt $((i))
+# 	# save metric
+# 	python3 ExponentMetric.py kseller $((i))
+# 	#echo 4/$i
+# done
+
+for i in 2 4 8 16
+do 
+	
+	mpiexec -n 3 python3 example_runMCMConModel.py input_refKuromoto.txt $((i))
+	# Run Optimization
+	python3 example_optMCMConModel.py input_refKuromoto.txt $((i))
+	# save metric
+	python3 ExponentMetric.py refKuromoto $((i))
+	#echo 4/$i
+done
+
+for i in 2 4 8 16
+do 
+	
+	mpiexec -n 3 python3 example_runMCMConModel.py input_alpcurve.txt $((i))
+	# Run Optimization
+	python3 example_optMCMConModel.py input_alpcurve.txt $((i))
+	# save metric
+	python3 ExponentMetric.py alpcurve $((i))
+	#echo 4/$i
+done
+
+
+# Plot Results for 2D Problems
+# python3 showPoints2D.py
+
+# Plot Results for 3D Problems
+# python3 showPoints3D.py
