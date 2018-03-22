e49dcb9ae27ad932d017b295b93ed5870bf7e292
Tang <yiyit62@gmail.com>
2018-03-22 14:11:04 -0400

    Change data file dir

--- plotHist.py
+++ plotHist.py
@@ -6,7 +6,7 @@ import matplotlib.mlab as mlab
 from res_3R5P import eval
 def plotres():
 	#load data
-	x = np.loadtxt('paramTraces_bvp.txt',skiprows=1)
+	x = np.loadtxt('Datafiles/paramTraces_3R5P.txt',skiprows=1)
 	res = np.empty([x.shape[0],24])
 	for i in range(x.shape[0]):
 		res[i,:]=eval(x[i,2:])
@@ -17,7 +17,7 @@ def plotres():
 	for i in range(24):
 
 		ax = plt.subplot(4,6,i+1)
-		n, bins, patches = ax.hist(res[:,i], num_bins, normed=1, facecolor='blue', alpha=0.5)
+		n, bins, patches = ax.hist(res[:,i], num_bins, normed=0, facecolor='blue', alpha=0.5)
 		# Tweak spacing to prevent clipping of ylabel
 
 		plt.subplots_adjust(left=0.15)
@@ -28,7 +28,7 @@ def plotres():
 	plt.show()
 def plotfval(model):
 	path = str(os.getcwd())+'/Graphes'
-	x = np.loadtxt('paramTraces_'+str(model)+'.txt',skiprows=1)
+	x = np.loadtxt('Datafiles/paramTraces_'+str(model)+'.txt',skiprows=1)
 	x = x[:,1]
 	num_bins =100
 	n,bins,patches = plt.hist(x,num_bins,normed=1,facecolor = 'blue',alpha=0.5)
@@ -38,4 +38,5 @@ def plotfval(model):
 	plt.show()
 
 if __name__ == '__main__':
-	plotfval('karumoto')
+	#plotfval('karumoto')
+	plotres()
