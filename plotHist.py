b8f7c665088b7075f4f4f94432e3f0150e6dab36
Tang <yiyit62@gmail.com>
2018-03-23 15:32:39 -0400

    add plot box

--- plotHist.py
+++ plotHist.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from res_3R5P import eval
+import pandas
+import seaborn as sns
+
 def plotres():
 	#load data
 	x = np.loadtxt('Datafiles/paramTraces_3R5P.txt',skiprows=1)
@@ -36,7 +39,26 @@ def plotfval(model):
 	plt.title('Histogram of function value')
 	plt.savefig('Graphes/Distribution_%s.pdf' %model)
 	plt.show()
+def plotbox(res,var):
+	#load data
+	# x = np.loadtxt('Datafiles/paramTraces_3R5P.txt',skiprows=1)
+	
+	# res = np.empty([x.shape[0],24])
+	# for i in range(x.shape[0]):
+	# 	res[i,:]=eval(x[i,2:])
+	#Create data frame
+	resdf = pandas.DataFrame(res)
+	#colname =["e1","e2","e3","e4","e5","e6","e7","e8","e9",'e10',"e11","e12","e13","e14","e15","e16","e17","e18","e19",'e20','e21','e22','e23','e24']
+	colname =["e1","e2","e3","e4","e5","e6","e7","e8","e9",'e10',"e11","e12","e13","e14","e15","e16","e17","e18","e19",'e20','e21','e22','e23','e24']
+	resdf.columns = colname
+	sns.set_style("whitegrid")
+	ax = sns.boxplot(data = resdf)
+	ax.set(xlabel='equation',ylabel='residue',title="Residue Distribution %d" %var)
+	plt.savefig('Graphes/Res_3R5P_%d.pdf' %var)
 
-if __name__ == '__main__':
-	#plotfval('karumoto')
-	plotres()
+	#plt.show()
+
+# if __name__ == '__main__':
+# 	#plotfval('alpcurve')
+# 	#plotres()
+# 	plotbox()
